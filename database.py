abe8d6bcd48dc9dd03a86092a5863c25ba511804
LaShaylaM35 <lashaylam@yahoo.com>
2026-05-04 19:30:39 -0500

    second commit

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ def get_connection():
         port=os.getenv("DB_PORT"),
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
+        password=os.getenv("DB_PASSWORD"),
         sslmode=os.getenv("DB_SSLMODE"),
     )
 
